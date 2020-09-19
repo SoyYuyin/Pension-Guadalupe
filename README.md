7a220181c59403b222886fb08e094003021cce83
SoyYuyin <emtzhd@gmail.com>
2020-09-18 20:05:48 -0500

    removed ejs. Added js file to index.html which runs a fetch funcion, parses json from our own api and injects html to generate cards

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Pension-Guadalupe
 Web site for La Pensión de Guadalupe
 ## Work in progress
+
+
+El servidor de express manda el archivo en endpoint get local host /,
+desde el documento html contiene una script tag en la cual, se corre una 
+funcion con fetch para consultar nuestro api.
+
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 //create express app
 const app = express();
@@ -9,11 +10,23 @@ app.set('view engine', 'ejs')
 //listen for requests
 app.listen(3000);
 
+app.use(morgan('dev'));
+
+lugares = [
+  {"lugar":"bodega", "descripcion":"400 m2, con baño y mas cosas"},
+  {"lugar":"Esta informacion", "descripcion":"viene de mi servidor de express"},
+  {"lugar":"local", "descripcion":"Sobre avenida principal, con bañoooooo"}
+]
+
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   // res.send('Hello worlddd')
-  res.render('index');
+  res.sendFile('/views/index.html', {root:__dirname});
   // res.sendFile(
 
 });
 
+app.get('/lugares', (req, res) => {
+  res.send(lugares)
+})
