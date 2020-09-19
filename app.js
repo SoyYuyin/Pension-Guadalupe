c3525f6f24650acdcfc0b60813af2faf911e81a4
SoyYuyin <emtzhd@gmail.com>
2020-09-19 18:47:10 -0500

    Connection with MongoDB Atlas through Mongoose successful

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const Space = require('./models/space');
 
 //create express app
 const app = express();
 
-//register view engine
-app.set('view engine', 'ejs')
+// stablish connection to mongogb using mongoose
+const dbURI = 'mongodb+srv://soyyuyin:XwBVF0JRHMkUkYYf@soyyuyin.02szu.mongodb.net/pension?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then((result) => {app.listen(3000)}) //we start listening for connections only after the connection to the db is successful
+.catch((err) => console.log(err));
 
-//listen for requests
-app.listen(3000);
+
+
+//register view engine
+// app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
 
@@ -19,6 +27,23 @@ lugares = [
 ]
 
 app.use(express.static('public'))
+
+app.get('/add-space', (req,res) =>{
+  const space = new Space({
+    space:'Lugar de estacionamiento, sin techo',
+    details: 'Dentro de la pensión con guardia de seguridad',
+    availability: true
+  });
+
+  space.save() //this is asynchronous
+    .then((result)=> {
+      res.send(result)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+});
+
 
 app.get('/', (req, res) => {
   // res.send('Hello worlddd')
